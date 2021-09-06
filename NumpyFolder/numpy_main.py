a1d7a1b5970575bf04e492c39b06d48290cd1468
David Enoma <davidenoma@gmail.com>
2021-09-06 17:28:40 +0100

    matplotlib_run

--- NumpyFolder/numpy_main.py
+++ NumpyFolder/numpy_main.py
@@ -1,12 +1,13 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 x = np.array([1,3,4], np.int16)
 y = np.ones(5)
 z = np.array([[1,2,3,5],[7,2,24,2]])
 z2 = np.array([[[1,2,3,4],[1,2,3,4]],[[-2,4,1,2],[5,6,3,1]]])
 def main():
     #arrays()
-    matrices()
+    #()
+    matplotlib_run()
 
 def arrays():
     print(y)
@@ -24,5 +25,12 @@ def matrices():
     print(y)
     print(x )
     print(x*y)
+
+def matplotlib_run():
+    x = np.arange(5)
+    y = x
+
+    plt.plot(x,y,'o')
+    plt.show()
 if __name__ == "__main__":
   main()
